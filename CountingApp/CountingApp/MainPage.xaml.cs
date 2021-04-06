7324bc097a4152289cedfde7ae003c91dd3fbccc
jhall41 <jhall41@my.wctc.edu>
2021-04-05 19:57:38 -0600

    First

---
+++ CountingApp/CountingApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CountingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            int weight = Int32.Parse(Inp_weight.Text);
+            int height = Int32.Parse(Inp_height.Text);
+            int BMI = (int)(weight / Convert.ToDouble(height *height) * 703);
+            Lbl_BMI.Text = $"Your BMI is {BMI}";
+        }
+    }
+}
